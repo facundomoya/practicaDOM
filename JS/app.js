844acf09f0a50257a637c9367078e483bf01fced
Facundo <facundomoya18@hotmail.com>
2021-09-17 13:39:24 -0300

    se sigue resolviendo los ejercicios del tp de practica de DOM y BOM

--- JS/app.js
+++ JS/app.js
@@ -12,38 +12,47 @@ function cambiarTitulo() {
 function verMas() {
   console.log("Prueba desde ver más");
   // buscar el boton (CADA VEZ QUE BUSCAMOS SE CREA UNA VARIABLE NUEVA)
-  let btnVermas=document.getElementById("btnVerMas")
+  let btnVermas = document.getElementById("btnVerMas");
   //crear y agregar un elemento al DOM
   //paso 1 - buscar el elemento padre
   let articulos = document.getElementsByTagName("article");
   console.log(articulos[1]);
-  
-  if(btnVermas.innerHTML == "Ver más..."){
-      btnVermas.innerHTML = "Ocultar"
-      btnVermas.className="btn btn-outline-danger"
 
-  //paso 2 - crear el elemento hijo
-  let parrafo = document.createElement("p"); //<p></p> crea una etiqueta p como si fuera un objeto
+  if (btnVermas.innerHTML == "Ver más...") {
+    btnVermas.innerHTML = "Ocultar";
+    btnVermas.className = "btn btn-outline-danger";
+
+    //paso 2 - crear el elemento hijo
+    /* let parrafo = document.createElement("p"); //<p></p> crea una etiqueta p como si fuera un objeto
   console.log(parrafo);
   parrafo.innerHTML =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error veritatis ad a nemo possimus saepe officiis. Corporis odit esse, vitae, tempora temporibus, officiis magni facilis repellat aliquam nam itaque consequuntur numquam. Velit adipisci illo debitis qui, sequi hic nobis beatae, placeat optio minima odio quas vel quibusdam. Necessitatibus, amet nemo. Error quasi placeat quod molestias ex sint, facilis alias facere numquam nesciunt id ducimus vitae totam dignissimos nostrum assumenda fugit? Ratione itaque dolore ex eveniet quis hic harum totam. Doloremque, praesentium. Nulla dolorum ea autem officia magni repudiandae enim perferendis, odit doloribus alias, delectus voluptas, labore ut voluptate itaque eveniet.";
   parrafo.className = "lead"
 
   //paso 3 - agregar el nuevo elemento hijo a su respectivo padre
-  articulos[1].appendChild(parrafo)
-  }else {
-      console.log("ahora cambiar el texto del boton por Ver más...")
-      //volver el boton a su estado original ver mas...
-      btnVermas.innerHTML="Ver más..."
-      btnVermas.className = "btn btn-outline-success"
-      //eliminar el parrafo
-      
-      console.log(articulos[1].hasChildNodes())
-      console.log(articulos[1].children.length)
-      console.log(articulos[1].children)
-      if(articulos[1].hasChildNodes() && articulos[1].children.length==3){
-          //eliminar efectivamente el parrafo
-          articulos[1].removeChild(articulos[1].children[2])
-      }
+  articulos[1].appendChild(parrafo) */
+
+    //segunda FORMA PARA CREAR ELEMENTOS HTML desde JS
+    
+    let parrafo = `<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus atque ad sit dignissimos, iste quia laboriosam neque hic eum.</p>`;
+    
+    //siempre que tengamos codigo de HTML hay que ponerlo entre comillas para que sea valido, JS no entiende HTML
+    articulos[1].innerHTML += parrafo 
+    // articulos[1].innerHTML = articuos[!].innerHTML + parrafo
+  
+  } else {
+    console.log("ahora cambiar el texto del boton por Ver más...");
+    //volver el boton a su estado original ver mas...
+    btnVermas.innerHTML = "Ver más...";
+    btnVermas.className = "btn btn-outline-success";
+    //eliminar el parrafo
+
+    console.log(articulos[1].hasChildNodes());
+    console.log(articulos[1].children.length);
+    console.log(articulos[1].children);
+    if (articulos[1].hasChildNodes() && articulos[1].children.length == 3) {
+      //eliminar efectivamente el parrafo
+      articulos[1].removeChild(articulos[1].children[2]);
+    }
   }
 }
---
+++ JS/ejercicio2.js
@@ -0,0 +1,64 @@
+class Persona{
+    constructor(nombre,edad,dni,sexo,peso,altura,nacimiento){
+        this.nombre=nombre
+        this.edad=edad
+        this.dni=dni
+        this.sexo=sexo
+        this.peso=peso
+        this.altura=altura
+        this.nacimiento=nacimiento
+    }
+}
+
+let persona = new Persona()
+
+function mostrarDatos(){
+    let prop1 = document.getElementById("input1")
+    this.nombre = prop1.value
+    console.log("Nombre: "+prop1.value)
+    let prop2 = document.getElementById("input2")
+    this.edad = prop2.value
+    console.log("Edad: "+prop2.value)
+    let prop3 = document.getElementById("input3")
+    this.dni=prop3.value
+    console.log("DNI: "+prop3.value)
+    let prop4 = document.getElementById("input4")
+    this.sexo = prop4.value
+    console.log("Sexo: "+prop4.value)
+    let prop5 = document.getElementById("input5")
+    this.peso = prop5.value
+    console.log("Peso: "+prop5.value)
+    let prop6 = document.getElementById("input6")
+    this.altura = prop6.value
+    console.log("Altura: "+prop6.value)
+    let prop7 = document.getElementById("input7")
+    this.nacimiento =prop7.value
+    console.log("Año de nacimiento: "+prop7.value)
+}
+
+
+function esMayordeEdad(){
+    if(this.edad >= 18){
+        alert("La persona es mayor de edad")
+    }else{
+        alert("La persona no es mayor de edad")
+    }
+}
+
+function mostrarGeneracion() {
+    if (this.nacimiento >= 1994 && this.nacimiento <= 2010) {
+        alert("La generación a la que pertenece es la GENERACION Z y su rasgo caracteristico es la irreverencia");
+      } else if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
+        alert(
+          "La generación a la que pertenece es la GENERACION Y (millennials) y su rasgo caracteristico es la frustación"
+        );
+      } else if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
+        alert("La generación a la que pertenece es la GENERACION X y su rasgo caracteristico es la obsesión por el éxito");
+      } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+        alert("La generación a la que pertenece es la Baby Boom y su rasgo caracteristico es la ambición");
+      } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+        alert(
+          "La generación a la que pertenece es la Silent Generation(los niños de la posguerra y su rasgo caracteristico es la austeridad"
+        );
+      }
+}
