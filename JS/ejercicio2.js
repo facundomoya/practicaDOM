e35d3467d833deeee94741aaa9d988c8b74aa3aa
Facundo <facundomoya18@hotmail.com>
2021-09-20 23:19:25 -0300

    se corrigio partes del ejercicio 2 y se lo termino

--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -1,68 +1,129 @@
-class Persona{
-    constructor(nombre,edad,dni,sexo,peso,altura,nacimiento){
-        this.nombre=nombre
-        this.edad=edad
-        this.dni=dni
-        this.sexo=sexo
-        this.peso=peso
-        this.altura=altura
-        this.nacimiento=nacimiento
+class Persona {
+  constructor(nombre, edad, dni, sexo, peso, altura, nacimiento) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.sexo = sexo;
+    this.peso = peso;
+    this.altura = altura;
+    this.nacimiento = nacimiento;
+  }
+
+  // Metodos
+
+  mostrarDatos() {
+    let datosImpresos = document.getElementById("datosImpresos");
+
+    datosImpresos.innerHTML = `
+      <h1 class="display-4 mb-4">Datos de la persona</h1>
+      <div class="lead">
+        <p>Nombre: ${this.nombre}</p>
+        <p>Edad: ${this.edad}</p>
+        <p>DNI: ${this.dni}</p>
+        <p>Sexo: ${this.sexo}</p>
+        <p>Peso: ${this.peso}</p>
+        <p>Altura: ${this.altura}</p>
+        <p>Año de nacimiento: ${this.nacimiento}</p>
+      </div>
+      `;
+  }
+
+  esMayordeEdad() {
+    if (this.edad >= 18) {
+      alert("La persona es mayor de edad");
+    } else if (this.edad < 18) {
+      alert("La persona no es mayor de edad");
+    } else {
+      alert("La edad ingresada no existe");
     }
-}
+  }
 
-
-function mostrarDatos(){
-    let prop1 = document.getElementById("input1")
-    this.nombre = prop1.value
-    console.log("Nombre: "+prop1.value)
-    let prop2 = document.getElementById("input2")
-    this.edad = prop2.value
-    console.log("Edad: "+prop2.value)
-    let prop3 = document.getElementById("input3")
-    this.dni=prop3.value
-    console.log("DNI: "+prop3.value)
-    let prop4 = document.getElementById("input4")
-    this.sexo = prop4.value
-    console.log("Sexo: "+prop4.value)
-    let prop5 = document.getElementById("input5")
-    this.peso = prop5.value
-    console.log("Peso: "+prop5.value)
-    let prop6 = document.getElementById("input6")
-    this.altura = prop6.value
-    console.log("Altura: "+prop6.value)
-    let prop7 = document.getElementById("input7")
-    this.nacimiento =prop7.value
-    console.log("Año de nacimiento: "+prop7.value)
-
-}
-
-function crearObjeto(){
-  let person = new Persona(nombre,edad,dni,sexo,peso,altura,nacimiento)
-  console.log(person)
-}
-
-function esMayordeEdad(){
-    if(this.edad >= 18){
-        alert("La persona es mayor de edad")
-    }else{
-        alert("La persona no es mayor de edad")
-    }
-}
-
-function mostrarGeneracion() {
+  mostrarGeneracion() {
     if (this.nacimiento >= 1994 && this.nacimiento <= 2010) {
-        alert("La generación a la que pertenece es la GENERACION Z y su rasgo caracteristico es la irreverencia");
-      } else if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
-        alert(
-          "La generación a la que pertenece es la GENERACION Y (millennials) y su rasgo caracteristico es la frustación"
-        );
-      } else if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
-        alert("La generación a la que pertenece es la GENERACION X y su rasgo caracteristico es la obsesión por el éxito");
-      } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
-        alert("La generación a la que pertenece es la Baby Boom y su rasgo caracteristico es la ambición");
-      } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
-        alert(
-          "La generación a la que pertenece es la Silent Generation(los niños de la posguerra y su rasgo caracteristico es la austeridad"
-        );
-      }
+      alert(
+        "La generación a la que pertenece es la GENERACION Z y su rasgo caracteristico es la irreverencia"
+      );
+    } else if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
+      alert(
+        "La generación a la que pertenece es la GENERACION Y (millennials) y su rasgo caracteristico es la frustación"
+      );
+    } else if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
+      alert(
+        "La generación a la que pertenece es la GENERACION X y su rasgo caracteristico es la obsesión por el éxito"
+      );
+    } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+      alert(
+        "La generación a la que pertenece es la Baby Boom y su rasgo caracteristico es la ambición"
+      );
+    } else if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+      alert(
+        "La generación a la que pertenece es la Silent Generation(los niños de la posguerra y su rasgo caracteristico es la austeridad"
+      );
+    }
+  }
 }
+
+//Creo una variable global para guardar aqui la persona creada
+let nuevaPersona;
+
+// Funcion para crear y mostrar el objeto en pantalla
+let CrearMostrarPersona = () => {
+  //Un boton de tipo submit por defecto tiene un evento que es validar los datos de los inputs y recargar la pagina luego de presionarlo, como no queremos que eso ocurra ya que si se actualiza la pagina borraria nuestros datos escritos usamos "event.preventDefault()" esto evita que se ejectute el evento del boton y por lo tanto no recargara la pagina
+  event.preventDefault();
+
+  //Traigo los inputs desde el html
+  let nombre = document.getElementById("input1").value;
+  let edad = document.getElementById("input2").value;
+  let dni = document.getElementById("input3").value;
+  let sexo = document.getElementById("input4").value;
+  let peso = document.getElementById("input5").value;
+  let altura = document.getElementById("input6").value;
+  let nacimiento = document.getElementById("input7").value;
+
+  // En este if pregunto si hay algun input vacio, si es que lo hay, informo al usuario mediante un aletr que llene todos los campos para continuar y por ultimo pongo un "return" para frenar el codigo y no dejar que siga, de lo contrario si estan todos los campos llenos no se ejecuta la condicional y continua con el codigo
+  if (
+    nombre == "" ||
+    edad == "" ||
+    dni == "" ||
+    sexo == "" ||
+    peso == "" ||
+    altura == "" ||
+    nacimiento == ""
+  ) {
+    alert("Para continuar debe llenar todos los campos");
+    return;
+  }
+
+  //Creo el objeto persona
+  let objetoNuevaPersona = new Persona(
+    nombre,
+    edad,
+    dni,
+    sexo,
+    peso,
+    altura,
+    nacimiento
+  );
+
+  //Guardo la persona creada en la variable global
+  nuevaPersona = objetoNuevaPersona;
+
+  //Ejecuto el metodo mostrarDatos() de la persona creada
+  nuevaPersona.mostrarDatos();
+
+  //Traigo el div que contiene los botones "mostrar generacion" y "mostrar si es mayor de edad"
+  const botonesPersona = document.getElementById("botonesPersona");
+
+  //Elimino la clase "d-none" del contenedor de los botones para que los mismos aparezcan en pantalla una vez que se haya creado la persona correctamente
+  botonesPersona.classList.remove("d-none");
+};
+
+// Funcion para ejecutar el metodo esMayordeEdad()
+let funcionEsMayordeEdad = () => {
+  nuevaPersona.esMayordeEdad();
+};
+
+// Duncion para ejectutar el metodo mostrarGeneracion()
+let funcionMostrarGeneracion = () => {
+  nuevaPersona.mostrarGeneracion();
+};
